2aadf77be0253ee977952dec166d630af6b574c5
Nicholas Lopez <Lopez4163@gmail.com>
2023-11-08 21:28:37 -0600

    2 comp

---
+++ challenges.js
@@ -0,0 +1,16 @@
+// * Complete the 'aVeryBigSum' function below.
+//  *
+//  * The function is expected to return a LONG_INTEGER.
+//  * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+//  */
+
+function aVeryBigSum(ar) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += ar[i]
+    }
+
+    return sum
+
+}
+
