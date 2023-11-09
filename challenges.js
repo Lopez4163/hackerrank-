7d89c0e2d012b4ac48e45f0eceb8ade6b3bf09f1
Nicholas Lopez <Lopez4163@gmail.com>
2023-11-08 21:36:25 -0600

    2nd comp

--- challenges.js
+++ challenges.js
@@ -14,3 +14,24 @@ function aVeryBigSum(ar) {
 
 }
 
+// * Complete the 'hurdleRace' function below.
+// *
+// * The function is expected to return an INTEGER.
+// * The function accepts following parameters:
+// *  1. INTEGER k
+// *  2. INTEGER_ARRAY height
+// */
+
+function hurdleRace(k, height) {
+   //i want to get the highest number in the array and subtracy it by k
+    for(let i=0; i < height.length; i++) {
+        if(k > Math.max(...height)){
+            return 0
+        }
+        else {
+               return Math.max(...height) - Math.min(k)
+        }
+           
+    }
+
+}
